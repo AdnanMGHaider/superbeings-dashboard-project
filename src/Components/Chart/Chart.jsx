3a4d9b7699299f1031aad3f2780d8ccb0b1e69cd
Adnan <68817284+AdnanH1006@users.noreply.github.com>
2022-04-27 20:30:30 +0600

    layout changes

--- src/Components/Chart/Chart.jsx
+++ src/Components/Chart/Chart.jsx
@@ -33,8 +33,8 @@ function Chart({ superBeingPowerstats }) {
   return (
     <div className={classes.chart}>
       <h3 className={classes.chartHeading}>POWER STATISTICS</h3>
-      <ResponsiveContainer width="100%" aspect={2 / 1}>
-        <BarChart width={500} height={300} data={superBeingPowerStats} margin={{ top: 5, right: 30, left: 20, bottom: 5 }}>
+      <ResponsiveContainer aspect={2 / 1}>
+        <BarChart className={classes.barChart} data={superBeingPowerStats}>
           <CartesianGrid strokeDasharray="3 3" />
           <XAxis dataKey="name" />
           <YAxis domain={[0, 100]}></YAxis>
